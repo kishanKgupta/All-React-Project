28c4033dde88888bcd545a551c025c89deb15f08
kishan gupta <kkgupta0106@gmail.com>
2025-03-01 17:39:29 +0530

    Add icon

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
-import "./Header.css"
+import "./Header.css";
+import { FaReact } from "react-icons/fa";
 
 const Header = () => {
   return (
-      <div className='header'>
-      <p>Reacr Project</p>
-      <nav className='nav-bar'>
+    <div className="header">
+      <FaReact style={{fontSize:"50px"}}/>
+      <nav className="nav-bar">
         <p>Contact</p>
         <p>Blog</p>
         <p>Resume</p>
       </nav>
-     </div>
-  )
+    </div>
+  );
 }
 
 export default Header;
