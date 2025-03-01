6c8cbad1f26738d4bd2d6523b79df0ff75c58323
kishan gupta <kkgupta0106@gmail.com>
2025-03-02 01:19:39 +0530

    improve css

--- Project/01_counter/Counter.jsx
+++ Project/01_counter/Counter.jsx
@@ -1,38 +1,35 @@
 import { useState } from "react";
+// import "./Counter.css";
 
-function Counter() {
+function App() {
   const [count, setCount] = useState(0);
 
-  function handleCounter(action) {
-    setCount((prev) => {
-      if (action === "increment") return prev >= 10 ? 0 : prev + 1;
-      if (action === "decrement") return prev <= 1 ? 0 : prev - 1;
-      return prev;
-    });
+  function math1(value) {
+    if (value == "increment") {
+      if (count >= 10) {
+        setCount(0);
+        return;
+      }
+      setCount(count + 1);
+    } else if (value == "decrement") {
+      if (count <= 1) {
+        setCount(0);
+        return;
+      }
+      setCount(count - 1);
+    } else {
+      console.log("Give real value!");
+    }
   }
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
-      <div className="border border-gray-400 bg-white shadow-lg rounded-lg p-10 text-center">
-        <h1 className="text-2xl font-bold mb-4">Counter App</h1>
-        <h1 className="text-3xl font-semibold text-blue-600">{count}</h1>
-        <div className="mt-4 flex gap-4">
-          <button
-            onClick={() => handleCounter("increment")}
-            className="px-4 py-2 bg-green-600 text-white rounded-md hover:bg-green-600"
-          >
-            Increment
-          </button>
-          <button
-            onClick={() => handleCounter("decrement")}
-            className="px-4 py-2 bg-red-500 text-red-700 rounded-md hover:bg-red-600"
-          >
-            Decrement
-          </button>
-        </div>
+    <div className="flex items-center justify-center h-lvh">
+      <div className=" border-black border p-10">
+        <h1>Counter App</h1>
+        <h1>{count}</h1>
+        <button onClick={() => math1("increment")}>Increment</button>
+        <button onClick={() => math1("decrement")}>Decrement</button>
       </div>
     </div>
   );
 }
-
-export default Counter;
